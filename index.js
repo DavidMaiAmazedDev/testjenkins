cef356d0ae052ad633b8330b4e2699bd9c350ab4
duymai <rucro12345@gmai.com>
2021-06-16 09:22:37 +0700

    fix script/deploy2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
- return res.send("Hello World ok!");
+ return res.send("Hello World ok1!");
 });
 
 // listen to port 7000 by default
